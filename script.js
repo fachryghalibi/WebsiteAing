31278996769b5b52e57b966eeec56cf4b226fead
fachryghalibi <53090324+fachryghalibi@users.noreply.github.com>
2023-08-28 21:09:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const stars = document.getElementById('stars');
 const mountains_behind = document.getElementById('mountains_behind');
 const text = document.getElementById('text');
-const logoku = document.getElementById('logoku');
 const mountains_front = document.getElementById('mountains_front');
 const header = document.querySelector('header');
 
@@ -19,4 +18,5 @@ window.addEventListener('scroll', function() {
   text.style.marginTop = value * 1.5 + 'px';
   logoku.style.marginTop = value * 1.5 + 'px';
   header.style.top = value * 0.5 + 'px';
+  
 });
